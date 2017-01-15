922b4ffd493a7ca6da363725773f44bd55bccf5f
Joe Rees <joedrees@gmail.com>
2017-01-15 12:50:23 +0000

    made the page headers have H1s in the nice font

--- wp-content/themes/addictionaffiliates/page.php
+++ wp-content/themes/addictionaffiliates/page.php
@@ -12,7 +12,7 @@
   <?php while (have_posts()) : the_post(); ?>
     <article <?php post_class(); ?>>
       <header>
-        <h2 class="entry-title"><?php the_title(); ?></h2>
+        <h1 class="entry-title"><?php the_title(); ?></h2>
         <?php get_template_part('templates/entry-meta'); ?>
       </header>
       <div class="entry-summary">
