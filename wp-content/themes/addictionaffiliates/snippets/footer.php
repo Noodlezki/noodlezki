0c50d9f2c2931e5c80f03d56476ee3e5469cf46d
Joe Rees <joedrees@gmail.com>
2017-01-22 15:16:54 +0000

    added footer social links and fixed fontawesome in addictionaff

--- wp-content/themes/addictionaffiliates/snippets/footer.php
+++ wp-content/themes/addictionaffiliates/snippets/footer.php
@@ -7,7 +7,13 @@
         <p class="footer-contact-child">Phone: <a><?= get_phone(); ?></a></p>
         <? $email = get_email(); ?>
         <p class="footer-contact-child">Email: <a href="mailto: <?= $email; ?>"><?= $email; ?></a></p>
-        <!-- <p>Copyright &#38;copy 2016<?php echo date("Y") == "2016" ? "" : "&#45;".date("y"); ?> -->
+        <p>Follow us on social media</p>
+        <p class="footer-socials">
+          <a href="https://www.facebook.com/recoveryguide/"><i class="fa fa-facebook-official fa-2x"></i></a>
+          <a href="https://www.twitter.com/recoveryguide/"><i class="fa fa-twitter fa-2x"></i></a>
+          <a href="https://www.linkedin.com/recoveryguide/"><i class="fa fa-linkedin fa-2x"></i></a>
+        </p>
+        <!-- <p>Copyright &#38;copy 2016<?php echo date('Y') == '2016' ? '' : '&#45;' . date('y'); ?> -->
 
       </div>
     </div>
