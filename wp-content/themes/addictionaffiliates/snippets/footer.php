33da0be34668846ec092c9be793cae2d5e64e502
Joe Rees <joedrees@gmail.com>
2017-01-22 19:12:33 +0000

    updated footer social links on addiction

--- wp-content/themes/addictionaffiliates/snippets/footer.php
+++ wp-content/themes/addictionaffiliates/snippets/footer.php
@@ -9,9 +9,9 @@
         <p class="footer-contact-child">Email: <a href="mailto: <?= $email; ?>"><?= $email; ?></a></p>
         <p>Follow us on social media</p>
         <p class="footer-socials">
-          <a href="https://www.facebook.com/recoveryguide/"><i class="fa fa-facebook-official fa-2x"></i></a>
-          <a href="https://www.twitter.com/recoveryguide/"><i class="fa fa-twitter fa-2x"></i></a>
-          <a href="https://www.linkedin.com/recoveryguide/"><i class="fa fa-linkedin fa-2x"></i></a>
+          <a href="https://www.facebook.com/recoveryguide"><i class="fa fa-facebook-official fa-2x"></i></a>
+          <a href="https://www.twitter.com/recoveryguide1"><i class="fa fa-twitter fa-2x"></i></a>
+          <a href="https://www.linkedin.com/in/recovery-guide-26b035137"><i class="fa fa-linkedin fa-2x"></i></a>
         </p>
         <!-- <p>Copyright &#38;copy 2016<?php echo date('Y') == '2016' ? '' : '&#45;' . date('y'); ?> -->
 
