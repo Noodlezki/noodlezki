eccd67f8930e97981e2433f8b586c9de4a9642d1
Joe Rees <joedrees@gmail.com>
2017-01-27 00:02:17 +0000

    added social links to footer

--- wp-content/themes/twatamoron/snippets/footer.php
+++ wp-content/themes/twatamoron/snippets/footer.php
@@ -1,7 +1,19 @@
 <footer class="site-footer">
   <div class="container">
     <span id="footer-text">
-      Copyright &copy 2016<?php echo date("Y") == "2016" ? "" : "-".date("y"); ?>.
+      Contact Me
+      <br />
+      <!-- Email -->
+      <a href="mailto:twatamoron@outlook.com"><i class="fa fa-2x fa-envelope-o"></i></a>
+      <!-- Medium -->
+      <a href="https://medium.com/@twatamoron"><i class="fa fa-2x fa-medium"></i></a>
+      <!-- Instagram -->
+      <a href="https://instagram.com/twatamoron"><i class="fa fa-2x fa-instagram"></i></a>
+      <!-- twitter -->
+      <a href="https://www.twitter.com/twatamoron"><i class="fa fa-2x fa-twitter"></i></a>
+      <? $first_year = '2017'; $fy = $first_year; ?>
+      <br />
+      Copyright &copy <?= $fy; ?><?php echo date('Y') == $fy ? '' : '-' . date('y'); ?>.
       All rights reserved.
     </span>
   </div>
