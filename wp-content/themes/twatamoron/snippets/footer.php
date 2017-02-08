60bc2a2589ff50ed7a2483fc1c75a1485e4eaf64
Joe Rees <joedrees@gmail.com>
2017-02-08 23:17:33 +0000

    added facebook to footer

--- wp-content/themes/twatamoron/snippets/footer.php
+++ wp-content/themes/twatamoron/snippets/footer.php
@@ -11,6 +11,8 @@
       <a href="https://instagram.com/twatamoron"><i class="fa fa-2x fa-instagram"></i></a>
       <!-- twitter -->
       <a href="https://www.twitter.com/twatamoron"><i class="fa fa-2x fa-twitter"></i></a>
+      <!-- Facebook -->
+      <a href="https://www.facebook.com/The-World-According-to-a-Moron-1872477812992914"><i class="fa fa-2x fa-facebook"></i></a>
       <? $first_year = '2017'; $fy = $first_year; ?>
       <br />
       Copyright &copy <?= $fy; ?><?php echo date('Y') == $fy ? '' : '-' . date('y'); ?>.
