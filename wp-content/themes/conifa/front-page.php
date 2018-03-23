3e0bc0141e7b624ba7b565e2c7a0c609051c9b48
Joe Rees <joedrees@gmail.com>
2018-03-23 22:04:14 +0000

    made front page video look alrightish

--- wp-content/themes/conifa/front-page.php
+++ wp-content/themes/conifa/front-page.php
@@ -3,7 +3,7 @@
 ?>
 
 <main class="main main--front" role="main">
-  <div class="font-page__video-container">
+  <div class="front-page__video-container">
     <video autoplay loop muted class="front-page__video">
       <source src="<?= $video; ?>" />
     </video>
