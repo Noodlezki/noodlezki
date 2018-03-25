f05f6f71b5a40809ac8d7f19fca8d58f71280935
Joe Rees <joedrees@gmail.com>
2018-03-26 00:51:49 +0100

    introduced arrow to menu items

--- wp-content/themes/conifa/snippets/menu-item.php
+++ wp-content/themes/conifa/snippets/menu-item.php
@@ -7,6 +7,7 @@ function add_team($team) {
     <img class="menu-group__flag menu-group__flag--left" src="<?= get_stylesheet_directory_uri() . "/assets/flags/$teamImg.png"; ?>" />
     <a class="menu-group__link"><?= $team ?></a>
     <img class="menu-group__flag menu-group__flag--right" src="<?= get_stylesheet_directory_uri() . "/assets/flags/$teamImg.png"; ?>" />
+    <i class="menu-group__arrow fa fa-angle-down" aria-hidden="true"></i>
   </li>
 <?php
 }
