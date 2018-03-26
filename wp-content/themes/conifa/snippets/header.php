8199798e1d82e3bea1b6318357fe30f3e10bce5e
Joe Rees <joedrees@gmail.com>
2018-03-26 23:54:23 +0100

    single post header stuff

--- wp-content/themes/conifa/snippets/header.php
+++ wp-content/themes/conifa/snippets/header.php
@@ -1,8 +1,8 @@
 <?php
-  $thumbnail_url = has_post_thumbnail() ? the_post_thumbnail_url : get_stylesheet_directory_uri() . '/assets/img/default.jpeg';
+  $thumbnail_url = has_post_thumbnail() ? the_post_thumbnail_url : get_stylesheet_directory_uri() . '/assets/images/default.jpeg';
 ?>
 <header class="banner" role="banner" style="background-image: url(<?= $thumbnail_url; ?>)">;
   <div class="container">
-    <h2 class="post-title"><?php the_title(); ?></h2>
+    <h1 class="post-title"><?php the_title(); ?></h1>
   </div>
 </header>
