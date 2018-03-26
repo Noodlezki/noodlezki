f0e20866521d03d85ff7c91a4d7ce89875b4fbc1
Joe Rees <joedrees@gmail.com>
2018-03-26 23:40:17 +0100

    removed function taht doesn't need to be there

--- wp-content/themes/conifa/functions.php
+++ wp-content/themes/conifa/functions.php
@@ -5,17 +5,6 @@ require_once('lib/init.php');
 // --- Custom theme code past this line ---
 define("IS_PRODUCTION", get_option("siteurl")=="http://your.site.url");
 
-// Needed for article-list.php
-function get_the_post_thumbnail_src($img) {
-  return (preg_match('~\bsrc="([^"]++)"~', $img, $matches)) ? $matches[1] : '';
-}
-function get_the_post_thumbnail_width($img) {
-  return (preg_match('~\bwidth="([^"]++)"~', $img, $matches)) ? $matches[1] : '';
-}
-function get_the_post_thumbnail_height($img) {
-  return (preg_match('~\bheight="([^"]++)"~', $img, $matches)) ? $matches[1] : '';
-}
-
 function is_site_administrator() {
   if ( ! is_user_logged_in() ) {
     return false;
