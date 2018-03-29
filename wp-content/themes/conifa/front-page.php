2242a0dc50d9cb9cddb7d14e55130ec2528a5a2b
Joe Rees <joedrees@gmail.com>
2018-03-29 01:12:01 +0100

    getting on with the front page design for small screen.s md is bork

--- wp-content/themes/conifa/front-page.php
+++ wp-content/themes/conifa/front-page.php
@@ -17,7 +17,11 @@
     if (!array_key_exists($group, $groups)) {
       $groups[$group] = array();
     }
-    array_push($groups[$group], the_title(null, null, false));
+    array_push($groups[$group],
+      array(
+        'title' => the_title(null, null, false),
+        'permalink' => get_permalink()
+      ));
   endwhile;
   ksort($groups);
 ?>
@@ -25,11 +29,16 @@
 <main class="main main--front" role="main">
   <div class="front-page__video-container">
 
-    <?php foreach ($groups as $group => $teams) : sort($teams); ?>
+    <?php foreach ($groups as $group => $teams) :
+      usort($teams, function ($item1, $item2) {
+        return $item1['title'] <=> $item2['title'];
+      });
+    ?>
       <div class="menu-group menu-group--<?= ord($group) - 64; ?>">
+        <h3 class="menu-group__title">Group <?= $group; ?></h3>
         <ul class="menu-group__menu">
           <?php foreach ($teams as $team) : ?>
-            <?php add_team($team); ?>
+            <?php add_team($team['title'], $team['permalink']); ?>
           <?php endforeach; ?>
         </ul>
       </div>
--- wp-content/themes/conifa/snippets/menu-item.php
+++ wp-content/themes/conifa/snippets/menu-item.php
@@ -1,10 +1,10 @@
 <?php
-function add_team($team) {
+function add_team($team, $link) {
   $teamImg = htmlentities($team);
   $teamImg = str_replace(' ', '_', $teamImg);
   $teamImg = str_replace('&eacute;', 'e', $teamImg);
 ?>
-  <a href="/teams/<?= $teamImg; ?>">
+  <a href="<?= $link; ?>">
     <li class="menu-group__item">
       <img class="menu-group__flag menu-group__flag--left" src="<?= get_stylesheet_directory_uri() . "/assets/flags/$teamImg.png"; ?>" />
       <span class="menu-group__link"><?= $team ?></span>
