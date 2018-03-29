432eae0e22aab2f7eee280f1af4e1ca7b3ab3399
Joe Rees <joedrees@gmail.com>
2018-03-29 01:10:39 +0100

    conifa footer

--- wp-content/themes/conifa/snippets/footer.php
+++ wp-content/themes/conifa/snippets/footer.php
@@ -1,9 +1,16 @@
-<footer>
+<footer class="footer" role="footer">
+  <? $email = "joedrees@gmail.com"; ?>
+  <? $first_year = '2018'; ?>
   <div class="container">
     <div class="row">
       <div class="col-lg-12">
 
-        <p>Skeleton Theme - Give it a fork.</p>
+      <h6>Contact Me: <a href="mailto:<?= $email; ?>"><?= $email; ?></a></h6>
+
+      <span class="copyright">
+        Copyright &copy <?= $first_year; ?><?php echo date('Y') == $first_year ? '' : '-' . date('y'); ?>.
+        All rights reserved.
+      </span>
 
       </div>
     </div>
