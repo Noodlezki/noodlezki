e6ac5657bae76af45f9b81a3e1247d57b0f6479f
Joe Rees <joedrees@gmail.com>
2018-04-18 00:06:08 +0100

    added overlay to banner header

--- wp-content/themes/conifa/snippets/header.php
+++ wp-content/themes/conifa/snippets/header.php
@@ -1,9 +1,11 @@
 <?php
   $thumbnail_url = has_post_thumbnail() ? get_the_post_thumbnail_url() : get_stylesheet_directory_uri() . '/assets/images/default.jpeg';
 ?>
-<header class="banner" role="banner" style="background-image: url(<?= $thumbnail_url; ?>)">;
-  <div class="container">
-    <a href="/"><h1 class="post-title"><?php the_title(); ?></h1></a>
-    <a href="/"><h6 class="home-link">&laquo Home</h6></a>
+<header class="banner" role="banner" style="background-image: url(<?= $thumbnail_url; ?>)">
+  <div class="banner--cover">
+    <div class="container">
+      <a href="/"><h1 class="post-title"><?php the_title(); ?></h1></a>
+      <a href="/"><h6 class="home-link">&laquo Home</h6></a>
+    </div>
   </div>
 </header>
