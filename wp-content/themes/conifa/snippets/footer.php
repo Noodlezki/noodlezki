8aec7644c8a53ff980da9490e5f948d6c6e1de6e
Joe Rees <joedrees@gmail.com>
2018-05-09 02:31:06 +0100

    added a little bit more to the footer

--- wp-content/themes/conifa/snippets/footer.php
+++ wp-content/themes/conifa/snippets/footer.php
@@ -7,6 +7,9 @@
 
       <h6>Contact Me: <a href="mailto:<?= $email; ?>"><?= $email; ?></a></h6>
 
+      <h6>This site is in no way affiliated with either ConIFA or the sponsors of the 2018 ConIFA World Football Cup.</h6>
+      <h6>All views expressed throughout this website are my own, and for that I can only apologise.</h6>
+
       <span class="copyright">
         Copyright &copy <?= $first_year; ?><?php echo date('Y') == $first_year ? '' : '-' . date('y'); ?>.
         All rights reserved.
