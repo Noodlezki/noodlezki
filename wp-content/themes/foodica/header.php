accef881883d436ffe9dee3b9b5a4574fac99ddb
Joe Rees <joedrees@gmail.com>
2020-09-27 16:09:22 +0100

    added gtag to k's site

--- wp-content/themes/foodica/header.php
+++ wp-content/themes/foodica/header.php
@@ -14,6 +14,14 @@
 
 	<?php wp_head(); ?>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-67989873-3"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-67989873-3');
+    </script>
 </head>
 
 <body <?php body_class(); ?>>
